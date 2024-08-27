d8c98a0d5663eff73b629e1548dd038cf62c10cd
Sebastian guzman lizama <seb.anguz.li@gmail.com>
2024-08-26 22:11:29 -0400

    portafolio terminado

---
+++ script.js
@@ -0,0 +1,41 @@
+// funcion que aplica el estilo a la opcion seleccionada en e menu y quita la previamente seleccionado
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+//desaparece el menu una vez que se ha seleccionado una opcion en el modo responsive
+var x = document.getElementById("nav");
+x.className = "";
+
+}
+// funcion que muestra la responsividad del menu
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive"
+    }else{
+        x.className = ";"
+    }
+}
+
+// Detectar scrolling para aplicar la animación de las barras de habilidad
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//funcion que aplica la animacion de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >=300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
